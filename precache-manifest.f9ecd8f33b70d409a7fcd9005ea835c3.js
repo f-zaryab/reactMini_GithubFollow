5780bea12d1b372040583e76423611d13637457e
f-zaryab <zaryabfarooqui@gmail.com>
2022-09-30 02:28:43 +0500

    Updates

---
+++ precache-manifest.f9ecd8f33b70d409a7fcd9005ea835c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35100de66557f71b76a143b93f8b2c9",
+    "url": "/reactMini_GithubFollow/index.html"
+  },
+  {
+    "revision": "eccff471a5ba442348f6",
+    "url": "/reactMini_GithubFollow/static/css/main.980c564c.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/reactMini_GithubFollow/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactMini_GithubFollow/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eccff471a5ba442348f6",
+    "url": "/reactMini_GithubFollow/static/js/main.79d124b3.chunk.js"
+  },
+  {
+    "revision": "f35c00d4a9fa17700ebd",
+    "url": "/reactMini_GithubFollow/static/js/runtime-main.7dddfb1c.js"
+  }
+]);
